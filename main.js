766bae9e1726fa6eb8380fd20e7a30d2735e5a4b
Eduardo Fernandes <ceduardodfernandes@gmail.com>
2024-07-12 19:47:38 -0300

    Fix reference to astar.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { visualizeBFS } from "./modules/bfs.js";
 import { visualizeDFS } from "./modules/dfs.js";
-import { visualizeAStar } from "./modules/a_star.js";
+import { visualizeAStar } from "./modules/astar.js";
 
 const canvas = document.getElementById("visualizationCanvas");
 const ctx = canvas.getContext("2d");
